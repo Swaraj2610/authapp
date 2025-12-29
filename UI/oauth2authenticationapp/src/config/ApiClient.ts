22ed1b8e7c6cfdaa3b6359617cc98db1e8efae37
Swaraj Hanumant Akhade <akhadeswaraj1326@gmail.com>
2025-12-29 19:25:04 +0530

    UI update

--- UI/oauth2authenticationapp/src/config/ApiClient.ts
+++ UI/oauth2authenticationapp/src/config/ApiClient.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8083/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://authentication-production-7079.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
